f01d30b075c7e4443048665b0e26c2618b358037
Andrew Ty Nguyen <andyt.nguyen123@gmail.com>
2018-04-08 22:01:22 -0700

    simple app

--- redux/src/Containers/BookDetail.js
+++ redux/src/Containers/BookDetail.js
@@ -4,9 +4,15 @@ import { connect } from 'react-redux';
 class BookDetail extends Component {
 
 	render() {
+		if(this.props.activeBook.length === 0) {
+			return (
+				<h3>Select a book</h3>
+			);
+		}
+
 		return (
 			<div>
-				<h3>Book selected:</h3>
+				<h1>Details For:</h1>
 				<p>{this.props.activeBook.title}</p>
 			</div>
 		);
